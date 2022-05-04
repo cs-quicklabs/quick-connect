4df02eedf2567a549c9395cad02e616bab001c81
hansika17 <hansika.kalra171@gmail.com>
2022-05-04 17:49:25 +0530

    calls WIP

---
+++ db/migrate/20220504102618_create_calls.rb
@@ -0,0 +1,10 @@
+class CreateCalls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :calls do |t|
+      t.text :body
+      t.references :contact, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_35_053515) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_102618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_35_053515) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "calls", force: :cascade do |t|
+    t.text "body"
+    t.bigint "contact_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_calls_on_contact_id"
+    t.index ["user_id"], name: "index_calls_on_user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -174,6 +184,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_35_053515) do
     t.index ["user_id"], name: "index_users_on_user_id"
   end
 
+  add_foreign_key "calls", "contacts"
+  add_foreign_key "calls", "users"
   add_foreign_key "contacts", "accounts"
   add_foreign_key "contacts", "users"
   add_foreign_key "labels", "accounts"
