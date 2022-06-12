206437e7ead13c5613ff10658456b2239f68044c
hansika17 <hansika.kalra171@gmail.com>
2022-06-12 19:22:02 +0530

    issue fixed

--- app/policies/api/contact/timeline_policy.rb
+++ app/policies/api/contact/timeline_policy.rb
@@ -1,2 +1,6 @@
 class Api::Contact::TimelinePolicy < Api::Contact::BaseContactPolicy
+  def index?
+    return true if record.second.user == user
+    return false if record.second.archived?
+  end
 end
