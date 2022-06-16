b906aed5528ba99ad071ad2cff56fcd450def81f
hansika17 <hansika.kalra171@gmail.com>
2022-06-16 12:49:55 +0530

    qa issues fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,7 +16,7 @@ class SearchController < BaseController
 
     like_keyword = "%#{params[:q]}%".split(/\s+/)
     @profile = params[:profile]
-    @contacts = @current_user.contactss.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+    @contacts = @current_user.contacts.where("first_name iLIKE ANY ( array[?] )", like_keyword)
       .or(Contact.for_current_account.where("last_name iLIKE ANY ( array[?] )", like_keyword))
       .limit(4).order(:first_name)
 
