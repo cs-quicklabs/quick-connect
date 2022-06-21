7cfc950ea96071972f6fb8bbb26043682d863cba
hansika17 <hansika.kalra171@gmail.com>
2022-06-21 14:47:23 +0530

    validation added for string format

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,6 +12,10 @@ class Contact < ApplicationRecord
   scope :archived, -> { where(archived: true) }
   scope :active, -> { where(archived: false) }
   scope :available, -> { where(archived: false) }
+  validates :first_name, format: { with: /\A[a-zA-Z]+\z/,
+                                   message: " allows only letters" }
+  validates :last_name, format: { with: /\A[a-zA-Z]+\z/,
+                                  message: " allows only letters" }
   validates :phone, :presence => true,
                     :numericality => true,
                     :length => { :minimum => 10, :maximum => 12 }
