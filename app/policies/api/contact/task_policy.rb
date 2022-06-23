624ded7e3e104d9f36c441b692188d848d7c97aa
hansika17 <hansika.kalra171@gmail.com>
2022-06-23 15:50:33 +0530

    contact details issues fixed

--- app/policies/api/contact/task_policy.rb
+++ app/policies/api/contact/task_policy.rb
@@ -1,9 +1,2 @@
 class Api::Contact::TaskPolicy < Api::Contact::BaseContactPolicy
-  def edit?
-    return true if !record.third.completed
-  end
-
-  def update?
-    edit?
-  end
 end
--- app/policies/contact/task_policy.rb
+++ app/policies/contact/task_policy.rb
@@ -1,9 +1,2 @@
 class Contact::TaskPolicy < Contact::BaseContactPolicy
-  def edit?
-    return true if !record.second.completed
-  end
-
-  def update?
-    edit?
-  end
 end
