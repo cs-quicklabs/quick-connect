27ea2381a5608414360bc6be184bebc2f0fbc8db
hansika17 <hansika.kalra171@gmail.com>
2022-06-28 17:45:59 +0530

    delete user handles

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ApplicationRecord
   ACTIONS = ["created", "task", "unarchived", "event", "note", "call", "archived", "relation", "journal", "commented", "release_note", "conversation", "debt", "deleted"].freeze
   acts_as_tenant :account
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :trackable, polymorphic: true, optional: true
   belongs_to :eventable, polymorphic: true, optional: true
-  validates_presence_of :user, :action
+  validates_presence_of :action
   validates :action, inclusion: { in: ACTIONS }
 end
