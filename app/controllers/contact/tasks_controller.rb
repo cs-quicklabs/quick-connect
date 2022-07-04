09c522099f3e6043c03a1a67cac9bae5d412fe15
hansika17 <hansika.kalra171@gmail.com>
2022-07-04 12:00:37 +0530

    routes update

--- app/controllers/contact/tasks_controller.rb
+++ app/controllers/contact/tasks_controller.rb
@@ -38,6 +38,7 @@ class Contact::TasksController < Contact::BaseController
   end
 
   def create
+    binding.irb
     authorize [@contact, Task]
 
     @task = AddTask.call(task_params, current_user, @contact).result
