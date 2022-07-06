ce33244ca605d39874dedb58408ef2bb3a80cb8b
hansika17 <hansika.kalra171@gmail.com>
2022-07-06 09:24:54 +0530

    Update conversation.rb

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :user
-  validates_presence_of :date, :field_id
+  validates_presence_of :date, :field_id, :body
   belongs_to :field
   belongs_to :contact
   normalize_attribute :body, :with => :strip
