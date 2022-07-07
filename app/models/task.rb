f80418faef4eb9d8ad77ade06f6753924c0bbcee
hansika17 <hansika.kalra171@gmail.com>
2022-07-07 12:05:52 +0530

    fixed fixed

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   validates_presence_of :title, :due_date
   belongs_to :contact
   normalize_attribute :title, :body, :with => :strip
+  after_destroy_commit { broadcast_remove_to(Task) }
 end
