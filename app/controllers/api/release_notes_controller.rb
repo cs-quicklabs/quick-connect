e91c56d05b6f2cb5359a12684796b53848fcaa1b
hansika17 <hansika.kalra171@gmail.com>
2022-07-08 18:06:34 +0530

    Update release_notes_controller.rb

--- app/controllers/api/release_notes_controller.rb
+++ app/controllers/api/release_notes_controller.rb
@@ -3,7 +3,7 @@ class Api::ReleaseNotesController < Api::BaseController
 
   def index
     authorize [:api, :release_note]
-    @pagy, @release_notes = pagy_nil_safe(params, ReleaseNote.all.published.order(created_at: :desc), items: LIMIT)
+    @pagy, @release_notes = pagy_nil_safe(params, ReleaseNote.published.order(created_at: :desc), items: LIMIT)
     render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @release_notes, message: " Release Notes were successfully retrived" }
   end
 
