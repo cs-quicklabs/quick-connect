a2f2b52c2c1c3880cf07e9b13e5ece2aa3b73c0f
hansika17 <hansika.kalra171@gmail.com>
2022-07-14 17:46:42 +0530

    reset user fixed

--- app/interactors/reset_user.rb
+++ app/interactors/reset_user.rb
@@ -47,11 +47,11 @@ class ResetUser < Patterns::Service
   end
 
   def delete_relations
-    Relation.where("account_id=? AND 'default'=?", user.account.id, "true").destroy_all
+    Relation.where("account_id=? AND 'default'=?", user.account.id, "false").destroy_all
   end
 
   def delete_fields
-    Field.where("account_id=? AND 'default'=?", user.account.id, "true").destroy_all
+    Field.where("account_id=? AND 'default'=?", user.account.id, "false").destroy_all
   end
 
   attr_reader :user
