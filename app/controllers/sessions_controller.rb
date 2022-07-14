98e8f4c0fcf00b3b9b633de20e3196c51f84d276
hansika17 <hansika.kalra171@gmail.com>
2022-07-14 11:19:48 +0530

    Delete sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < Devise::SessionsController
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    yield if block_given?
-    respond_to do |format|
-      format.html { redirect_to archived_contacts_path, status: :see_other, notice: "" }
-    end
-  end
-end
