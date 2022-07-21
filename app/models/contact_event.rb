987c928c6ec1a9c854eef49165d0d4a6340859d6
hansika17 <hansika.kalra171@gmail.com>
2022-07-21 12:48:53 +0530

    test cases completed

--- app/models/contact_event.rb
+++ app/models/contact_event.rb
@@ -2,4 +2,5 @@ class ContactEvent < ApplicationRecord
   belongs_to :contact, class_name: "Contact", foreign_key: "contact_id"
   belongs_to :life_event, class_name: "LifeEvent", foreign_key: "life_event_id"
   validates_presence_of :title, :body
+  belongs_to :user
 end
--- test/system/contact/contact_activities_test.rb
+++ test/system/contact/contact_activities_test.rb
@@ -10,14 +10,14 @@ class ContactContactActivitiesTest < ApplicationSystemTestCase
   end
 
   def page_url
-    contact_tasks_url(script_name: "/#{@account.id}", contact_id: @contact.id)
+    contact_contact_activities_url(script_name: "/#{@account.id}", contact_id: @contact.id)
   end
 
   test "can visit index page if logged in" do
     visit page_url
     take_screenshot
     assert_selector "h1", text: "#{@contact.first_name} #{@contact.last_name}"
-    assert_text "Add New Task"
+    assert_text "Add Activity"
   end
 
   test "can not visit index page if not logged in" do
@@ -26,78 +26,66 @@ class ContactContactActivitiesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Sign in to your account"
   end
 
-  test "can see task detail page" do
+  test "can add new activity" do
     visit page_url
-    task = @contact.tasks.first
-    find("tr", id: dom_id(task)).click_link(task.title)
-    assert_selector "h3", text: task.title
+    fill_in "contact_activity_title", with: "Some Random Activity Title"
+    fill_in "contact_activity_body", with: "Some Random Activity Body"
+    fill_in "contact_activity_date", with: Time.now
+    select activities(:simple_one).name.upcase_first, from: "contact_activity_activity_id"
+    click_on "Add Activity"
+    assert_selector "#contact_activities", text: "Some Random Activity Title"
     take_screenshot
   end
 
-  test "can add new task" do
+  test "can not add activity with empty params" do
     visit page_url
-    fill_in "task_title", with: "Some Random Task Title"
-    fill_in "task_body", with: "Some Random Task Body"
-    fill_in "task_due_date", with: Time.now
-    click_on "Add Task"
-    assert_selector "tbody#tasks", text: "Some Random Task Title"
-    take_screenshot
-  end
-
-  test "can check a task" do
-    visit page_url
-    task = @contact.tasks.first
-
-    find("tr", id: dom_id(task)).check "completed"
-    take_screenshot
-  end
-
-  test "can not add task with empty params" do
-    visit page_url
-    click_on "Add Task"
+    click_on "Add Activity"
     take_screenshot
     assert_selector "div#error_explanation", text: "Title can't be blank"
   end
 
-  test "can edit a task" do
+  test "can edit a activity" do
     visit page_url
-    task = @contact.tasks.where(completed: 0).first
-    assert_text task.title
-    find("tr", id: dom_id(task)).click_link("Edit")
+    activity = @contact.contact_activities.first
+    assert_text activity.title
+    find(id: dom_id(activity)).click_link("Edit")
     take_screenshot
-    fill_in "task_title", with: "Task Edited"
-    click_on "Edit Task"
+    within "#contact_activities" do
+      fill_in "contact_activity_title", with: "Activity Edited"
+      click_on "Edit Activity"
+    end
     take_screenshot
-    assert_no_text "Edit Task"
-    assert_selector "p.notice", text: "Task was successfully updated."
-    assert_selector "tr##{dom_id(task)}", text: "Task Edited"
+    assert_no_text "Edit Activity"
+    assert_selector "##{dom_id(activity)}", text: "Activity Edited"
   end
 
-  test "can not edit task with invalid params" do
+  test "can not edit activity with invalid params" do
     visit page_url
-    task = @contact.tasks.where(completed: 0).first
-    assert_text task.title
-    find("tr", id: dom_id(task)).click_link("Edit")
-    fill_in "task_title", with: ""
-    click_on "Edit Task"
+    activity = @contact.contact_activities.first
+    assert_text activity.title
+    find(id: dom_id(activity)).click_link("Edit")
+    within "#contact_activities" do
+      fill_in "contact_activity_title", with: ""
+      click_on "Edit Activity"
+    end
     take_screenshot
     assert_selector "div#error_explanation", text: "Title can't be blank"
   end
 
-  test "can delete a task" do
+  test "can delete a activity" do
     visit page_url
-    task = @contact.tasks.where(completed: 0).first
-    display_name = task.contact.decorate.display_name
+    activity = @contact.contact_activities.first
+    display_name = activity.contact.decorate.display_name
     assert_text display_name
     page.accept_confirm do
-      find("tr", id: dom_id(task)).click_link("Delete")
+      find(id: dom_id(activity)).click_link("Delete")
     end
-    assert_no_selector "tbody#tasks", text: task.title
+    assert_no_selector "tbody#contact_activities", text: activity.title
   end
 
-  test "can not show add task when contact is inactive" do
+  test "can not show add activity when contact is inactive" do
     inactive_contact = contacts(:archived)
-    visit contact_tasks_url(script_name: "/#{@account.id}", contact_id: inactive_contact.id)
-    assert_no_text "Add New Task"
+    visit contact_contact_activities_url(script_name: "/#{@account.id}", contact_id: inactive_contact.id)
+    assert_no_text "Add New Activity"
   end
 end
