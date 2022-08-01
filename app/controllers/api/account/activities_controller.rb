b093af7272fbe703df6bc3a9916fe33e51a9eb34
hansika17 <hansika.kalra171@gmail.com>
2022-08-01 14:20:06 +0530

    array fixed

--- app/controllers/api/account/activities_controller.rb
+++ app/controllers/api/account/activities_controller.rb
@@ -3,7 +3,7 @@ class Api::Account::ActivitiesController < Api::Account::BaseController
 
   def index
     authorize [:api, :account]
-    @activities = Activity.all.includes(:group).order(:name).order(created_at: :desc).group_by { |r| r.group.slice(:name) }
+    @activities = Activity.all.includes(:group).order(:name).order(created_at: :desc).group_by { |r| r.group.name }.to_a
     render json: { success: true, data: @activities, message: "Activities were successfully retrieved." }
   end
 
