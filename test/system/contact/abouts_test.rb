cb638e41bb786afbc2774b186758845df1fd566e
hansika17 <hansika.kalra171@gmail.com>
2022-08-05 12:15:53 +0530

    test cases completed

--- test/system/contact/abouts_test.rb
+++ test/system/contact/abouts_test.rb
@@ -10,29 +10,50 @@ class ContactAboutsTest < ApplicationSystemTestCase
   end
 
   def page_url
-    project_about_url(script_name: "/#{@account.id}", project_id: @project.id)
+    contact_abouts_url(script_name: "/#{@account.id}", contact_id: @contact.id)
   end
 
-  test "can visit projects if logged in" do
+  test "can visit contatcts about if logged in" do
     visit page_url
-    assert_selector "h1", text: "#{@project.name}"
+    assert_selector "h1", text: "#{@contact.decorate.display_name}"
     assert_text "About"
   end
 
-  test "can edit the Project About" do
+  test "can edit the contact about" do
     visit page_url
-    find("turbo-frame").click_link("Edit")
-    fill_in "project_about", with: "This is test description about project"
-    click_on "Save"
-    sleep(0.5)
-    assert_text "This is test description about project"
+    about = @contact.abouts.slice(:address, :breif, :met, :habit, :work, :other)
+
+    about.each do |key, value|
+      frame_id = dom_id(@contact.abouts, key + "_turbo_frame")
+      within "##{frame_id}" do
+        click_on("Update")
+        fill_in "about_#{key}", with: "This is test about the contact"
+        click_on "Save"
+        sleep(0.5)
+        assert_text "This is test about the contact"
+      end
+    end
     take_screenshot
   end
 
-  test "can not visit project if not logged in" do
-    sign_out @employee
-
+  test "can not visit contact about if not logged in" do
+    sign_out @user
     visit page_url
     assert_selector "h1", text: "Sign in to your account"
+  end
+
+  test "can edit the contact delete" do
+    visit page_url
+    about = @contact.abouts.slice(:address, :breif, :met, :habit, :work, :other)
+    about.each do |key, value|
+      frame_id = dom_id(@contact.abouts, key + "_turbo_frame")
+      within "##{frame_id}" do
+        page.accept_confirm do
+          click_on("Delete")
+        end
+        assert_text "Not added"
+      end
+    end
+    take_screenshot
   end
 end
