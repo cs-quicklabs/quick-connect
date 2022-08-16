41749d19a3c8f9ea87297435bddbeaf7f553ee92
hansika17 <hansika.kalra171@gmail.com>
2022-08-16 17:11:42 +0530

    d

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -2,6 +2,7 @@ import ApplicationController from './application_controller.js'
 
 export default class extends ApplicationController {
   increment(event) {
+    winddow.location('73/contacts/24/batches')
     this.stimulate('Batch#show', event.target)
   }
 }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_controller.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
+  config.reload_classes_only_on_change = false
   # Do not eager load code on boot.
   config.eager_load = false
   config.assets.debug = true
