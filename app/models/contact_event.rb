392df81fe7949a456f233a9a4c0568c5cc3080a8
hansika17 <hansika.kalra171@gmail.com>
2022-08-16 17:39:13 +0530

    d

--- app/models/contact_event.rb
+++ app/models/contact_event.rb
@@ -1,7 +1,7 @@
 class ContactEvent < ApplicationRecord
   belongs_to :contact, class_name: "Contact", foreign_key: "contact_id"
-  belongs_to :life_event, class_name: "LifeEvent", foreign_key: "life_event_id"
   validates_presence_of :title, :body
+  belongs_to :life_event, class_name: "LifeEvent", foreign_key: "life_event_id"
   belongs_to :user
   validates :title,
             :length => { :maximum => 25 }
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,8 @@
 class Conversation < ApplicationRecord
   belongs_to :user
-  validates_presence_of :date, :body
-  belongs_to :field
   belongs_to :contact
+  belongs_to :field
+  validates_presence_of :date, :body
+
   normalize_attribute :body, :with => :strip
 end
