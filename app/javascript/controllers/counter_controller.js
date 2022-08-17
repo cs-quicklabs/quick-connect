d9f3d79ccfb24f789520f69c4d3f3e3857e0dc32
hansika17 <hansika.kalra171@gmail.com>
2022-08-17 13:43:34 +0530

    done

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -1,8 +1,14 @@
 import ApplicationController from './application_controller.js'
+import consumer from "../channels/consumer"
 
 export default class extends ApplicationController {
   increment(event) {
     consumer.connection.open()
+    let element = document.querySelector("meta[name='current-account']")
+    if (element == null) return
+    this.current_account = element.getAttribute('content')
+    window.addEventListener('page:change', function(event) {
     this.stimulate('Batch#show', event.target)
+    });
   }
 }
