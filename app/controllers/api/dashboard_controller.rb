37046e5b5894fdcb5fddf16af2e909983b17b80b
hansika17 <hansika.kalra171@gmail.com>
2022-08-23 12:10:36 +0530

    pagination added in favorites

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -7,8 +7,8 @@ class Api::DashboardController < Api::BaseController
 
   def recents
     authorize [:api, :dashboard]
-    @contacted = Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").where(events: { trackable_type: "PhoneCall" }).order(created_at: :desc).limit(5) +
-                 Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").where(events: { trackable_type: "Conversation" }).order(created_at: :desc).limit(5)
+    @contacted = Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").where(events: { trackable_type: "PhoneCall" }).order(created_at: :desc).limit(4) +
+                 Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").where(events: { trackable_type: "Conversation" }).order(created_at: :desc).limit(4)
     render json: { success: true, data: @contacted.as_json(:include => [:eventable, :trackable]), message: "Recents were successfully retrieved." }
   end
 
