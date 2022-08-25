9aa5ca80fa7d87160870336f6eb5f54d00063e67
hansika17 <hansika.kalra171@gmail.com>
2022-08-25 13:21:52 +0530

    fixed issues

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < BaseController
   def index
     authorize :favorite
-    @pagy, @favorites = pagy_nil_safe(params, Contact.all.favorites, items: LIMIT)
+    @pagy, @favorites = pagy_nil_safe(params, Contact.all.available.favorites, items: LIMIT)
     render_partial("contacts/contact", collection: @favorites) if stale?(@favorites)
   end
 end
