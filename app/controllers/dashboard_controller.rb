28b9ddf81ee3ecad2dbc3204ea3b77ce12afc3bc
hansika17 <hansika.kalra171@gmail.com>
2022-08-26 15:58:55 +0530

    upcoming issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,8 @@ class DashboardController < BaseController
     @reminders.each do |reminder|
       @upcoming_reminders += reminder.upcoming
     end
-    @upcoming_reminders = @upcoming_reminders.group_by { |r| r.second[:reminder].beginning_of_month }
+    binding.irb
+    @upcoming_reminders = @upcoming_reminders.sort_by { |r| r.second[:reminder] }.group_by { |r| r.second[:reminder].beginning_of_month }
     @contacted = Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "PhoneCall" }).order(created_at: :desc).limit(4) +
                  Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" }).order(created_at: :desc).limit(4)
   end
