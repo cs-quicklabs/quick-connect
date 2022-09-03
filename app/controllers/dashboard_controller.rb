f6885b88e8898eabbfb6a4b8e55d1eff4eab48fc
hansika17 <hansika.kalra171@gmail.com>
2022-09-03 11:59:50 +0530

    recents

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,8 +7,8 @@ class DashboardController < BaseController
       @upcoming_reminders += reminder.upcoming
     end
     @upcoming_reminders = @upcoming_reminders.sort_by { |r| r.second[:reminder] }.group_by { |r| r.second[:reminder].beginning_of_month }
-    @contacted = (Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").select("DISTINCT ON(events.action) events.*").where("contacts.archived=?", false).where(events: { trackable_type: "PhoneCall" }).limit(4) +
-                  Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").select("DISTINCT ON(events.action) events.*").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" }).limit(4)).sort_by { |r| r.created_at }.uniq { |r| r.eventable }
+    @contacted = (Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "PhoneCall" }).limit(4) +
+                  Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" }).limit(4)).sort_by { |r| r.created_at }.uniq { |r| r.eventable }
   end
 
   def events
