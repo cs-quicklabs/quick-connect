91aaa7f76d40094a0edd0c6e3bf5d364dfb65e4f
hansika17 <hansika.kalra171@gmail.com>
2022-09-06 17:42:37 +0530

    js fixed

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -2,11 +2,12 @@ import ApplicationController from './application_controller.js'
 import consumer from "../channels/consumer"
 
 export default class extends ApplicationController {
-  increment(event) {
+  increement(event) {
    
     let element = document.querySelector("meta[name='current-user']")
     if (element == null) return
     this.current_user = element.getAttribute('content')
+    console.log(this.current_user)
     consumer.connection.open()
    
   }
