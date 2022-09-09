3cd58d33d416e916608099fa1f7dcc4439b40bee
hansika17 <hansika.kalra171@gmail.com>
2022-09-09 14:51:16 +0530

    issues fixed

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Api::DashboardController < Api::BaseController
   def index
     authorize [:api, :dashboard]
-    @events = Event.all.includes(:eventable, :trackable).where("user_id IS NOT NULL").order(created_at: :desc).limit(50).decorate
+    @events = Event.all.includes(:eventable, :trackable).where("user_id IS NOT NULL").order(created_at: :desc).limit(50)
     render json: { success: true, data: @events.as_json(:include => [:eventable, :trackable, :account, :user]), message: "Events were successfully retrieved." }
   end
 
