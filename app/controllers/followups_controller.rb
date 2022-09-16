6d9060e7411c1990465d94a53af2e562ab5e80f6
hansika17 <hansika.kalra171@gmail.com>
2022-09-16 15:42:18 +0530

    followup fixed

--- app/controllers/followups_controller.rb
+++ app/controllers/followups_controller.rb
@@ -1,7 +1,7 @@
 class FollowupsController < BaseController
   def index
     authorize :followup
-    followups = Contact.all.available.tracked.joins("INNER JOIN events ON contacts.id = events.eventable_id").select("contacts.*, events.created_at as event_create, events.action as event_action, events.trackable_id as event_track, events.trackable_type as event_type").where("events.action IN (?)", ["called", "conversation", "contact_activity", "contact_event"]).where("events.trackable_id IS NOT NULL").order("events.created_at DESC").uniq
+    followups = Contact.all.available.tracked.joins("INNER JOIN events ON contacts.id = events.eventable_id").select("contacts.*,events.id as event, events.created_at as event_create, events.action as event_action, events.trackable_id as event_track, events.trackable_type as event_type").where("events.action IN (?)", ["called", "conversation", "contact_activity", "contact_event"]).where("events.trackable_id IS NOT NULL").order("events.created_at DESC").uniq
     @firsts = []
     @seconds = []
     @thirds = []
