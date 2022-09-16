8aa820190fb75f31dc3414ffff2d04032d91f35f
hansika17 <hansika.kalra171@gmail.com>
2022-09-16 12:14:32 +0530

    push origin dev

--- db/migrate/20220916063012_add_column_to_reminders.rb
+++ db/migrate/20220916063012_add_column_to_reminders.rb
@@ -1,5 +1,5 @@
 class AddColumnToReminders < ActiveRecord::Migration[7.0]
   def change
-    add_reference :reminders, :account, null: false, foreign_key: true
+    add_reference :reminders, :account, null: true, foreign_key: true
   end
 end
