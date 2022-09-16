94dd1a3211cb5bc4c8ccc7762feac9489ca29d19
hansika17 <hansika.kalra171@gmail.com>
2022-09-16 11:45:32 +0530

    Revert "dashboard WIP"
    This reverts commit 110373604472c9512d5cfbe48e284c4eb680e61b.

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -8,9 +8,9 @@ class Api::DashboardController < Api::BaseController
   def recents
     authorize [:api, :dashboard]
 
-    @contacted = Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "PhoneCall" }) +
-                 Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" })
-    render json: { success: true, data: @contacted.sort_by { |r| r.trackable.created_at }.uniq { |r| r.eventable }.take(8).as_json(:include => [:eventable, :trackable]), message: "Recents were successfully retrieved." }
+    @contacted = Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "PhoneCall" }).limit(4) +
+                 Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" }).limit(4)
+    render json: { success: true, data: @contacted.sort_by { |r| r.trackable.created_at }.uniq { |r| r.eventable }.as_json(:include => [:eventable, :trackable]), message: "Recents were successfully retrieved." }
   end
 
   def favorites
