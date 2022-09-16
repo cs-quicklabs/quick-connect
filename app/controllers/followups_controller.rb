c64d9b64a38749276ce1ff297de19beee8cbed8c
hansika17 <hansika.kalra171@gmail.com>
2022-09-16 12:44:57 +0530

    dashboard

--- app/controllers/followups_controller.rb
+++ app/controllers/followups_controller.rb
@@ -1,5 +1,21 @@
 class FollowupsController < BaseController
   def index
     authorize :followup
+    followups = Contact.all.joins("INNER JOIN events ON contacts.id = events.eventable_id").select("contacts.*,events.created_at as event_create, events.action as event_action, events.trackable_id as event_track").where("events.action IN (?)", ["phone_call", "conversation", "contact_activity", "contact_event"])
+    @firsts = []
+    @seconds = []
+    @thirds = []
+    @fourths = []
+    followups.each do |followup|
+      if Date.today >= followup.event_create && followup.event_create <= Date.today - 30.days
+        @firsts += [followup]
+      elsif Date.today - 30.days > followup.event_create && followup.event_create <= Date.today - 60.days
+        @seconds += [followup]
+      elsif Date.today - 60.days > followup.event_create && followup.event_create <= Date.today - 90.days
+        @thirds += [followup]
+      else
+        @fourths += [followup]
+      end
+    end
   end
 end
