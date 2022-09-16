588cdb4a2988a42e46e967fb18227090e4d32dd5
hansika17 <hansika.kalra171@gmail.com>
2022-09-16 12:48:18 +0530

    dashboard

--- app/controllers/api/followups_controller.rb
+++ app/controllers/api/followups_controller.rb
@@ -1,13 +1,13 @@
 class Api::FollowupsController < Api::BaseController
   def index
     authorize [:api, :followup]
-    followups = Contact.all.joins("INNER JOIN events ON contacts.id = events.eventable_id").select("DISTINCT ON (events.eventable_id)*").select("contacts.*, events.created_at as event_create, events.action as event_action, events.trackable_id as event_track").where("events.action IN (?)", ["phone_call", "conversation", "contact_activity", "contact_event"])
+    followups = Contact.all.tracked.joins("INNER JOIN events ON contacts.id = events.eventable_id").select("contacts.*, events.created_at as event_create, events.action as event_action, events.trackable_id as event_track").where("events.action IN (?)", ["phone_call", "conversation", "contact_activity", "contact_event"]).where("events.trackable_id IS NOT NULL").order("events.created_at DESC").uniq
     @firsts = []
     @seconds = []
     @thirds = []
     @fourths = []
     followups.each do |followup|
-      if Date.today >= followup.event_create && followup.event_create >= Date.today - 30.days
+      if Date.today >= followup.event_create.to_date && followup.event_create.to_date >= Date.today - 30.days
         @firsts += [followup]
       elsif Date.today - 30.days > followup.event_create && followup.event_create >= Date.today - 60.days
         @seconds += [followup]
