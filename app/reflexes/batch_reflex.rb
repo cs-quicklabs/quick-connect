2648486acba91417564247e0ee4232012164c8fc
hansika17 <hansika.kalra171@gmail.com>
2022-09-20 11:12:14 +0530

    groups

--- app/reflexes/batch_reflex.rb
+++ app/reflexes/batch_reflex.rb
@@ -21,8 +21,8 @@ class BatchReflex < ApplicationReflex
     @contact = Contact.find(element.dataset["contact-id"])
     @batch = Batch.find(element.dataset["batch-id"])
     @batch = AddContactToGroup.call(@batch, current_user, @contact).result
-    html = render(partial: "batches/contact", locals: { batch: @batch, contacts: @batch.contacts.includes(:batches_contacts).order("batches_contacts.created_at DESC").uniq })
+    html = render(partial: "batches/contact", locals: { batch: @batch, contacts: @batch.contacts.includes(:batches_contacts).order("batches_contacts.created_at DESC").uniq, message: "contact added successfully to group", search: contact.decorate.display_name })
 
-    morph "#batch_contacts", "#{html}"
+    morph "#show1", "#{html}"
   end
 end
