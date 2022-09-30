4961fb2a784e4f4820d972fc93f925676b134ea5
hansika17 <hansika.kalra171@gmail.com>
2022-09-30 17:29:37 +0530

    groups fixed

--- app/reflexes/batch_reflex.rb
+++ app/reflexes/batch_reflex.rb
@@ -5,7 +5,6 @@ class BatchReflex < ApplicationReflex
     batch = Batch.find(element.dataset["batch-id"])
     contacts = batch.contacts.uniq
     html = render(partial: "batches/show", locals: { batch: batch, contacts: contacts })
-
     morph "#show1", "#{html}"
   end
 
@@ -13,7 +12,6 @@ class BatchReflex < ApplicationReflex
     contact = Contact.find(element.dataset["contact-id"])
     relatives = Relative.includes(:contact, :relation).where(first_contact_id: contact.id)
     html = render(partial: "batches/profile", locals: { contact: contact, relatives: relatives })
-
     morph "#profile", "#{html}"
   end
 
@@ -39,8 +37,11 @@ class BatchReflex < ApplicationReflex
   def destroy
     @batch = Batch.find(element.dataset["batch"])
     @batch = DestroyGroup.call(current_user, @batch).result
+    @batches = Batch.all.order(:name)
+    batch = render(partial: "batches/batch", locals: { batches: @batches })
     html = render(partial: "batches/show", locals: { batch: [], contacts: [] })
     profile = render(partial: "batches/profile", locals: { contact: "" })
+    morph "#batches", "#{batch}"
     morph "#show1", "#{html}"
     morph "#profile", "#{profile}"
   end
