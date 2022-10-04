b03d1d78bc958a2f16b199bedb5bec209f38cae6
hansika17 <hansika.kalra171@gmail.com>
2022-10-04 21:30:18 +0530

    archive contact fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
+  config.action_dispatch.default_headers["Cache-Control"] = "no-cache, no-store"
   #config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.perform_deliveries = true
