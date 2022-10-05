65912c18c88c47e17bc5b3d7301891462db8c6e8
hansika17 <hansika.kalra171@gmail.com>
2022-10-05 15:21:57 +0530

    issues

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Kutumb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.active_storage.variant_processor = :mini_magick
-    config.hosts.clear
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
