5c7a7a5ed3c72c8415c28263a9e017bb3269c50f
Aashish Dhawan <aashishdhawan@gmail.com>
2022-10-06 16:21:57 +0530

    refactor jobs

--- app/jobs/daily_reminder_mailer_job.rb
+++ app/jobs/daily_reminder_mailer_job.rb
@@ -5,18 +5,14 @@ class DailyReminderMailerJob < ApplicationJob
       ActsAsTenant.current_tenant = account
       users = User.where(account_id: account)
       users.each do |user|
-        if !user.nil?
-          if user.email_enabled
-            reminder_for_user = user.reminders
-            todays_reminders = []
-            if reminder_for_user.count > 0
-              reminder_for_user.each do |reminder|
-                todays_reminders += reminder.today
-              end
-              if todays_reminders.count > 0
-                DailyReminderMailer.with(user: user, reminders: todays_reminders).daily_reminder.deliver_now
-              end
-            end
+        if !user.nil? and user.email_enabled
+          reminder_for_user = user.reminders
+          todays_reminders = []
+          reminder_for_user.each do |reminder|
+            todays_reminders += reminder.today
+          end
+          if todays_reminders.count > 0
+            DailyReminderMailer.with(user: user, reminders: todays_reminders).daily_reminder.deliver_now
           end
         end
       end
