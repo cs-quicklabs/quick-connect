d5871ddf5c63b38bcb6f39ba3571120ec50fc042
hansika17 <hansika.kalra171@gmail.com>
2022-10-06 17:20:49 +0530

    dec merge

--- app/reflexes/batch_reflex.rb
+++ app/reflexes/batch_reflex.rb
@@ -25,9 +25,9 @@ class BatchReflex < ApplicationReflex
     @contact = Contact.find(element.dataset["contact-id"])
     @batch = Batch.find(element.dataset["batch-id"])
     @batch = AddContactToGroup.call(@batch, current_user, @contact).result
-    html = render(partial: "batches/contact", locals: { batch: @batch, contacts: @batch.contacts.includes(:batches_contacts).where("contacts.archived=?", false).order("batches_contacts.created_at DESC").uniq, message: "Contact added successfully to group" })
+    html = render(partial: "batches/show", locals: { batch: @batch, contacts: @batch.contacts.includes(:batches_contacts).where("contacts.archived=?", false).order("batches_contacts.created_at DESC").uniq, message: "Contact added successfully to group" })
     profile = render(partial: "batches/profile", locals: { contact: @contact, relatives: Relative.includes(:contact, :relation).where(first_contact_id: @contact.id) })
-    morph "#batch_contacts", "#{html}"
+    morph "#show1", "#{html}"
     morph "#profile", "#{profile}"
   end
 
