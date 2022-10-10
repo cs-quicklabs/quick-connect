862fb70d151484f4452f0fdd0d5887fb65698c67
hansika17 <hansika.kalra171@gmail.com>
2022-10-10 16:16:14 +0530

    authentication updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   respond_to :html
   protect_from_forgery with: :null_session, if: :json_request?
-  protect_from_forgery with: :exception, if: :json_request?
+  protect_from_forgery with: :exception, unless: :json_request?
 
   skip_before_action :verify_authenticity_token, if: :json_request?
 
