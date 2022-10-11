f97144034b4ec30bce31ee4ae4c58c8bbb1b8888
hansika17 <hansika.kalra171@gmail.com>
2022-10-11 12:01:17 +0530

    dashboard udpated

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,7 @@ class DashboardController < BaseController
     end
     @upcoming_reminders = @upcoming_reminders.sort_by { |r| r.second[:reminder] }
     @contacted = (Event.joins("INNER JOIN phone_calls ON phone_calls.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "PhoneCall" }) +
-                  Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" })).uniq { |r| r.eventable }.take(8)
+                  Event.joins("INNER JOIN conversations ON conversations.id = events.trackable_id").joins("INNER JOIN contacts ON contacts.id = events.eventable_id").where("contacts.archived=?", false).where(events: { trackable_type: "Conversation" })).uniq { |r| r.eventable }.take(10)
 
     @tasks = Task.all.order(created_at: :desc).limit(10)
   end
