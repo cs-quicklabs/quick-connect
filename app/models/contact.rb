2c2a49398dbe8bdaaf244d58dfe246da0d060707
hansika17 <hansika.kalra171@gmail.com>
2022-10-11 16:12:51 +0530

    task show fixed

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < ApplicationRecord
   scope :favorites, -> { where(favorite: true) }
   belongs_to :user
   belongs_to :account
-  validates_presence_of :first_name, :last_name, :intro, on: :create
+  validates_presence_of :first_name, :last_name, on: :create
   validates_uniqueness_of :phone, scope: :account, :allow_blank => true, on: :create
   validates_uniqueness_of :email, scope: :account, :allow_blank => true, on: :create
   has_many :notes, dependent: :destroy
--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -16,6 +16,13 @@ class TaskReflex < ApplicationReflex
     morph "#{dom_id(task)}", render(partial: "contact/tasks/task", locals: { task: task })
   end
 
+  def toggle_contact_task_dasboard
+    task = Task.find(element.dataset[:id])
+    task.update(completed: !task.completed)
+    TasksMailer.with(actor: task.user, contact: task.contact, task: task).completed_email.deliver_later if deliver_email?(task)
+    task.save!
+  end
+
   def deliver_email?(task)
     task.completed and task.user.email_enabled
   end
