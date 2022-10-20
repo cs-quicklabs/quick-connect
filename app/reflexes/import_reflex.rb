af3ad5136aadfcfe7c63bc2840b1534589fa3baa
hansika17 <hansika.kalra171@gmail.com>
2022-10-20 22:11:36 +0530

    Delete import_reflex.rb

--- app/reflexes/import_reflex.rb
+++
@@ -1,9 +0,0 @@
-class ImportReflex < ApplicationReflex
-  delegate :current_user, to: :connection
-
-  def save
-    binding.irb
-    result = Contact.insert_all(element.dataset["contacts"])
-    binding.irb
-  end
-end
