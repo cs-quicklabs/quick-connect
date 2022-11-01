ca4813ead1eaee558a7d228755859f700cb0e101
hansika17 <hansika.kalra171@gmail.com>
2022-11-01 17:33:54 +0530

    formatted

--- app/reflexes/contact_reflex.rb
+++ app/reflexes/contact_reflex.rb
@@ -6,7 +6,6 @@ class ContactReflex < ApplicationReflex
     contacts = Contact.for_current_account.available.order(:first_name)
     relatives = Relative.includes(:contact, :relation).where(first_contact_id: contact.id)
     html = render(partial: "contacts/profile", locals: { contact: contact, contacts: contacts, relatives: relatives })
-
     morph "#profile", "<div id='profile'>#{html}</div>"
   end
 
