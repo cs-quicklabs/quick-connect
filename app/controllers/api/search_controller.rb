ccc29d549e4443d5bb42a5c93fb173bcc164d5b3
hansika17 <hansika.kalra171@gmail.com>
2022-11-02 11:29:21 +0530

    pagination added in mobile search

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -3,12 +3,13 @@ class Api::SearchController < Api::BaseController
     authorize [:api, :search]
 
     like_keyword = "#{params[:q]}".split(/\s+/)
-    @contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+    contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
       .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
       .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
       .order(:first_name).uniq
+    @pagy, @contacts = pagy_array_safe(params, contacts, items: LIMIT)
 
-    render json: { success: true, data: @contacts, message: "" }
+    render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @contacts, message: "" }
   end
 
   def relative
@@ -17,12 +18,12 @@ class Api::SearchController < Api::BaseController
     like_keyword = "%#{params[:q]}%".split(/\s+/)
     @profile = params[:profile]
     @contact = [Contact.find(params[:profile])]
-    @contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+    contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
       .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
       .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
       .order(:first_name).uniq - @contact
-
-    render json: { success: true, data: @contacts, message: "" }
+    @pagy, @contacts = pagy_array_safe(params, contacts, items: LIMIT)
+    render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @contacts, message: "" }
   end
 
   def add
@@ -32,11 +33,11 @@ class Api::SearchController < Api::BaseController
     @batch = Batch.find(params[:batch_id])
     @added = @batch.contacts
 
-    @contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+    contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
       .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
       .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
       .order(:first_name).uniq - @added
-
-    render json: { success: true, data: @contacts, message: "" }
+    @pagy, @contacts = pagy_array_safe(params, contacts, items: LIMIT)
+    render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @contacts, message: "" }
   end
 end
