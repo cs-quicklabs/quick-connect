3743975b1804a153c5c3657ac3fe1345b9a1910c
hansika17 <hansika.kalra171@gmail.com>
2022-11-03 09:59:50 +0530

    nil search handled

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,12 +1,16 @@
 class Api::SearchController < Api::BaseController
   def contacts
     authorize [:api, :search]
+    if !q.nil?
+      like_keyword = "#{params[:q]}".split(/\s+/)
+      contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+        .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
+        .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
+        .order(:first_name).uniq
+    else
+      contacts = []
+    end
 
-    like_keyword = "#{params[:q]}".split(/\s+/)
-    contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
-      .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
-      .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
-      .order(:first_name).uniq
     @pagy, @contacts = pagy_array_safe(params, contacts, items: LIMIT)
 
     render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @contacts, message: "" }
@@ -14,29 +18,35 @@ class Api::SearchController < Api::BaseController
 
   def relative
     authorize [:api, :search]
-
-    like_keyword = "%#{params[:q]}%".split(/\s+/)
-    @profile = params[:profile]
-    @contact = [Contact.find(params[:profile])]
-    contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
-      .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
-      .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
-      .order(:first_name).uniq - @contact
+    if !q.nil?
+      like_keyword = "%#{params[:q]}%".split(/\s+/)
+      @profile = params[:profile]
+      @contact = [Contact.find(params[:profile])]
+      contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+        .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
+        .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
+        .order(:first_name).uniq - @contact
+    else
+      contacts = []
+    end
     @pagy, @contacts = pagy_array_safe(params, contacts, items: LIMIT)
     render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @contacts, message: "" }
   end
 
   def add
     authorize [:api, :search]
+    if !q.nil?
+      like_keyword = "%#{params[:q]}%".split(/\s+/)
+      @batch = Batch.find(params[:batch_id])
+      @added = @batch.contacts
 
-    like_keyword = "%#{params[:q]}%".split(/\s+/)
-    @batch = Batch.find(params[:batch_id])
-    @added = @batch.contacts
-
-    contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
-      .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
-      .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
-      .order(:first_name).uniq - @added
+      contacts = Contact.all.available.where("first_name iLIKE ANY ( array[?] )", like_keyword)
+        .or(Contact.all.available.where("last_name iLIKE ANY ( array[?] )", like_keyword))
+        .or(Contact.all.available.where("first_name iLIKE ANY ( array[?] ) and last_name iLIKE ANY ( array[?] )", like_keyword, like_keyword))
+        .order(:first_name).uniq - @added
+    else
+      contacts = []
+    end
     @pagy, @contacts = pagy_array_safe(params, contacts, items: LIMIT)
     render json: { pagy: pagination_meta(pagy_metadata(@pagy)), success: true, data: @contacts, message: "" }
   end
