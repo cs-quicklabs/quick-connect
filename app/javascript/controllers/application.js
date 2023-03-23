72f108e70f51af4584dfd2c7af742a49af5c6e0e
Aashish Dhawan <aashishdhawan@gmail.com>
2023-03-23 21:28:48 +0530

    install new relic

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,7 +5,7 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.warnings = true
-application.debug    = true
+application.debug    = false
 application.consumer = consumer
 window.Stimulus      = application
 
