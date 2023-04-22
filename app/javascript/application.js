9636af89ead437f18e23c18553ac9f07e0bc2d21
Aashish Dhawan <aashishdhawan@gmail.com>
2023-04-22 20:08:17 +0530

    WIP

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,15 @@
 // Entry point for the build script in your package.json
 
-import "./controllers"
-import "./channels"
-import "./config"
-import '@rails/actiontext'
-import '@rails/activestorage'
-import "@rails/actioncable"
-import '@tailwindcss/line-clamp'
-import '@tailwindcss/forms'
-import 'trix'
-import "./trix-editor-overrides"
-import "@hotwired/turbo-rails"
+import "./controllers";
+import "./channels";
+import "./config";
+import "@rails/actiontext";
+import "@rails/activestorage";
+import "@rails/actioncable";
+import "@tailwindcss/line-clamp";
+import "@tailwindcss/forms";
+import "trix";
+import "./trix-editor-overrides";
+import "./themes";
+import "./github-canvas";
+import "@hotwired/turbo-rails";
