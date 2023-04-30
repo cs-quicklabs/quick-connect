dac844ea6d4a7b6c2f929578f0c0b414191b3662
hansika17 <hansika.kalra171@gmail.com>
2023-04-30 17:22:52 +0530

    followup after changed

---
+++ app/interactors/update_touch_back.rb
@@ -0,0 +1,31 @@
+class UpdateTouchBack < Patterns::Service
+  def initialize(contact, actor, touch_back_after)
+    @contact = contact
+    @actor = actor
+    @touch_back_after = touch_back_after
+  end
+
+  def call
+    begin
+      update_touch_back
+      add_event
+    rescue
+      contact
+    end
+    contact
+  end
+
+  private
+
+  def update_touch_back
+    contact.touch_back_after = touch_back_after
+    contact.followup_after_changed_on = Date.today
+    contact.save!
+  end
+
+  def add_event
+    contact.events.create(user: actor, action: "touched", action_for_context: "updated follow up after", trackable: contact, action_context: "updated follow up after for this contact")
+  end
+
+  attr_reader :contact, :actor, :touch_back_after
+end
---
+++ app/javascript/controllers/followup_after_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["selected", "touched"];
+
+  toggleSelected(event) {
+    event.preventDefault();
+    const selected = this.selectedTargets.find(
+      (item) => item.dataset.selected === "true"
+    );
+
+    const item = event.target
+    item.dataset.selected = "true";
+    item.classList.remove("bg-white");
+    item.classList.add("bg-gray-50");
+
+    if (selected) {
+      selected.dataset.selected = "false";
+      selected.classList.remove("bg-gray-50");
+      selected.classList.add("bg-white");
+      const url = event.target.href
+
+      fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
+        .then((response) => response.text())
+        .then((html) => {
+          this.touchedTarget.innerHTML = html;
+        });
+
+    } else {
+      const url = event.target.href
+
+      fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
+        .then((response) => response.text())
+        .then((html) => {
+            this.touchedTarget.innerHTML = html;
+        });
+    }
+  }
+}
