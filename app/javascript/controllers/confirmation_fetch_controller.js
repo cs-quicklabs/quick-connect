39dc1afe4c1cf8c4e84efa4a2c41a91beca1302c
Aashish Dhawan <aashishdhawan@gmail.com>
2023-07-25 16:44:23 +0530

    code refactor

--- app/javascript/controllers/confirmation_fetch_controller.js
+++ app/javascript/controllers/confirmation_fetch_controller.js
@@ -1,24 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
- static values = { message: String }
- static targets = ["replace"]
+  static values = { message: String }
+  static targets = ["replace"]
 
   confirm(event) {
-     event.preventDefault()
+    event.preventDefault()
     if (!(window.confirm(this.messageValue))) {
-     
       event.stopImmediatePropagation()
-    } else{
-      const url= event.target.href
+    } else {
+      const url = event.target.href
       fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
         .then((response) => response.text())
         .then((html) => {
           this.replaceTarget.innerHTML = html;
         });
-        if (document.getElementById("profile")){
+      if (document.getElementById("profile")) {
         document.querySelector("#profile1").remove();
-        }
+      }
     }
   }
 }
