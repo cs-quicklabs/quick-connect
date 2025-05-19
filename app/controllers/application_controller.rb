a9ab90a08506750cfb422f88d948686f0e99e872
Aashish Dhawan <aashishdhawan@gmail.com>
2025-05-19 13:05:43 +0530

    remove subscription checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -115,12 +115,13 @@ class ApplicationController < ActionController::Base
   end
 
   def landing_path
-    expired_subscription = SubscriptionManager.new(current_user.account.owner).template == "expired"
-    if expired_subscription
-      current_user.account.expired = true
-      current_user.account.save!
-      return expired_path(script_name: script_name)
-    end
+    # Disable subscription management for now
+    # expired_subscription = SubscriptionManager.new(current_user.account.owner).template == "expired"
+    # if expired_subscription
+    #   current_user.account.expired = true
+    #   current_user.account.save!
+    #   return expired_path(script_name: script_name)
+    # end
 
     dashboard_path(script_name: script_name)
   end
