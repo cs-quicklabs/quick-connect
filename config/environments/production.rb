356740f2695f64b6e635482cbfce6ad3662b33b6
Aashish Dhawan <aashishdhawan@gmail.com>
2025-05-29 12:14:33 +0530

    disable callback erros

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_controller.raise_on_missing_callback_actions = false
 
   config.action_mailer.default_url_options = { :host => "https://connect.quicklabs.in" }
   # ActionMailer Config
