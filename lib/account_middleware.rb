28e6af86c60d867f9b84f9b8d8f4de69534a4e0e
Aashish Dhawan <aashishdhawan@gmail.com>
2025-06-09 16:38:02 +0530

    refactor account middleware

--- lib/account_middleware.rb
+++ lib/account_middleware.rb
@@ -4,26 +4,40 @@ class AccountMiddleware
   end
 
   def call(env)
-    request = ActionDispatch::Request.new env
-    _, account_id, request_path = env["REQUEST_PATH"].split("/", 3)
+    request = ActionDispatch::Request.new(env)
+    path_segments = env["REQUEST_PATH"].to_s.split("/", 3)
+    _, account_id, request_path = path_segments
 
-    if account_id =~ /\d+/
-      if account = Account.find_by(id: account_id)
+    if account_id&.match?(/^\d+$/)
+      account = Account.find(account_id)
+      if account
+        # Set the current account for ActsAsTenant and Current
         Current.account = account
         ActsAsTenant.current_tenant = account
-        if account.expired? and request_path != "expired" and not env["REQUEST_PATH"].include?("sign_out")
-          return [302, { "Location" => "/#{account.id}/expired" }, []]
+
+        if account.expired? && request_path != "expired" && !env["REQUEST_PATH"].include?("sign_out")
+          return redirect_to("/#{account.id}/expired")
         end
       else
-        return [302, { "Location" => "/" }, []]
+        return redirect_to("/")
       end
 
-      env["SCRIPT_NAME"] = "/#{account_id}"
-      env["PATH_INFO"] = "/#{request_path}"
-      env["REQUEST_PATH"] = "/#{request_path}"
-      env["REQUEST_URI"] = "/#{request_path}"
+      update_env_for_account(env, account_id, request_path)
     end
 
     @app.call(env)
+  end
+
+  private
+
+  def redirect_to(location)
+    [302, { "Location" => location, "Content-Type" => "text/html" }, []]
+  end
+
+  def update_env_for_account(env, account_id, request_path)
+    env["SCRIPT_NAME"] = "/#{account_id}"
+    env["PATH_INFO"] = "/#{request_path}"
+    env["REQUEST_PATH"] = "/#{request_path}"
+    env["REQUEST_URI"] = "/#{request_path}"
   end
 end
