2a2b9bf9a986fb76d8ca6024dcd219d4630b7eec
Aashish Dhawan <aashishdhawan@gmail.com>
2025-06-09 12:52:33 +0530

    remove link & hashtag extractors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
   include Pagy::Frontend
-  include AutoLinkHelper
-  include Extractor::HashTag
 
   def tailwind_form_with(**options, &block)
     form_with(**options.merge(builder: TailwindFormBuilder), &block)
