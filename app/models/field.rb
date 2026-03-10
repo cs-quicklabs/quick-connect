c2021594fbfaa005f5340adaa6306642a58ab75c
Aashish Dhawan <aashishdhawan@gmail.com>
2026-03-10 19:31:55 +0530

    fix erro

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,4 +1,6 @@
 class Field < ApplicationRecord
+  self.inheritance_column = nil
+
   acts_as_tenant :account
   validates_presence_of :name, :case_sensitive => false
   validates_uniqueness_to_tenant :name, :case_sensitive => false
